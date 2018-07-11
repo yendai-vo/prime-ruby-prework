565d4a0e6bce0c1e006311765dfa338aa1a79569
Cindy Vo <yendai.vo@hotmail.com>
2018-07-11 03:23:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
 def prime?(num)
   return false if num < 2
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  (2..Math.sqrt(num)).none? {|i| (num % i).zero?}
 end
